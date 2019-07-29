7895c02b0062877864fbd5dbd1561c17c5f4643c
chaerulzzz <chaerul.a12@gmail.com>
2019-07-29 13:17:42 +0700

    ganti local date

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:login_firebase/authentication/authentication.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-class HomeScreen extends StatelessWidget {
+String _setLocale() {
+  initializeDateFormatting("in_ID");
+  return DateFormat("EEEE d MMMM yyyy", "in_ID").format(DateTime.now());
+}
+
+class HomeScreen extends StatelessWidget {  
   final String name;
-
+  final String formattedDate = _setLocale();
   HomeScreen({Key key, @required this.name}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +29,7 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Center(child: Text('Welcome $name!'),)
+          Center(child: Text('Selamat Datang $name !\n $formattedDate', textAlign: TextAlign.center, ),)
         ],
       ),
     );
